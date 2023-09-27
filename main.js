e559d227d74217a60bc703524b4da2722967c1ec
cabravoac <49000086+cabravoac@users.noreply.github.com>
2023-09-26 19:22:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,75 +1,51 @@
-//  repaso
-/*
-let valorUno = parseFloat(prompt("ingrese un valor"))
-let valorDos = parseFloat(prompt("ingrese otro valor"))
-let operacion = prompt("ingrese una operación")
-
-while(valorUno || valorUno === 0){
-    let resultado = 0
-    switch(operacion){
-        case "+":
-            resultado = valorUno +  valorDos
-            break
-        case "-":
-            resultado = valorUno - valorDos
-            break
-        case "*":
-            resultado = valorUno * valorDos
-            break
-        case "/":
-            resultado = valorUno / valorDos
-            break 
-
-    }
-    alert(resultado)
-    valorUno = parseFloat(prompt("ingrese un valor"))
-    valorDos = parseFloat(prompt("ingrese otro valor"))
-    operacion = prompt("ingrese una operación")
-}
-*/
-
-
-
-// fin repaso
-
-
-/*
-function saludar (){
-    let nombre = prompt("ingrese una funcion")
-    alert("hola" + " " + nombre)
-}
-
-saludar()
-*/
+consultaClima()
 
 function consultaClima(){
 
+    const datosClima = [
+        {
+            ciudad: "CDMX",
+            abreviatura:"CDMX",
+            temperatura: 20,
+            velocidadViento: 150,
+        },
+        {
+            ciudad: "Bruselas",
+            abreviatura:"BRU",
+            temperatura: 25,
+            velocidadViento: 120,
+        },
+        {
+            ciudad: "Tokio",
+            abreviatura:"TK",
+            temperatura: 28,
+            velocidadViento: 100,
+        },
+        {
+            ciudad: "Vancouver",
+            abreviatura:"VAN",
+            temperatura: 18,
+            velocidadViento: 80,
+        },
+        {
+            ciudad: "Chicago",
+            abreviatura:"CHI",
+            temperatura: 30,
+            velocidadViento: 200,
+        }
+    ]
 
-    let pregunta1 = prompt("¿Te interesa saber el estado del clima en donde estás? (si/no)").toUpperCase()
+    let continuar = confirm("¿Te interesa saber el estado del clima en donde estás?")
 
-    while(pregunta1 === "SI") {        
-        nombreUsuario = prompt("Ingresa tu nombre").toUpperCase()
-        zonaPais = prompt("¿En qué pais te encuentras?").toUpperCase()
-        zonaUsuario = prompt("¿En qué zona vives? P.ej; Norte, Sur, Este, Oeste").toUpperCase()
-
-        if(zonaUsuario === "NORTE") {
-            alert(nombreUsuario + ", vistete fresco y mantente hidratado")
-        } else if(zonaUsuario === "SUR"){
-            alert(nombreUsuario + ", te recomendamos taparte por el frente frío #15 que se avecina")
-        } else if(zonaUsuario === "ESTE"){
-            alert(nombreUsuario + ", se esperan vientos fuertes y cielos despejados")
-        } else if(zonaUsuario === "OESTE"){
-            alert(nombreUsuario + ", evita salir a carretera ya que se espera una neblina espesa por las noches")
-        } else (
-            alert("Ingresa una región corecta")
-        )
-
-        pregunta1 = prompt("¿te interesaria saber otro clima?")
+    while(continuar){        
+        let nombreUsuario = prompt("Ingresa tu nombre").toUpperCase()
+        let ciudadIngresada = prompt("¿En qué ciudad vives? P.ej; CDMX,BRU,TK,VAN,CHI").toUpperCase()
+        console.log(ciudadIngresada)
+        let ciudadEncontrada = datosClima.find(dato => dato.abreviatura == ciudadIngresada)
+        alert("Hola!! " + nombreUsuario + " la temperatura en " + ciudadEncontrada.ciudad + " es de " + ciudadEncontrada.temperatura + " grados con vientos de " + ciudadEncontrada.velocidadViento + "km/h")
+        
+        continuar = confirm("¿Te interesa saber el estado del clima en donde estás?")
 
     } 
     alert("Gracias por visitarnos, vuelva pronto")
-
-}
-
-consultaClima()
-
+} 
